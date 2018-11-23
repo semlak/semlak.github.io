7f313b37bb2ac8200b085c53e31828415410e26b
Joseph <semlak@gmail.com>
2018-11-22 18:57:14 -0600

    working on PDF embeding

--- src/components/ResumeModal.js
+++ src/components/ResumeModal.js
@@ -11,12 +11,20 @@ import { Modal, ModalBody } from 'reactstrap';
 //
 // const validateEmail = email => !!email.match("[a-za-z]+.*@.*[a-za-z]+.*[.][a-za-z]+");
 
-export default props => (
-  <div className="pdf-modal-container">
-    <Modal size="lg" isOpen={props.isOpen} toggle={props.toggle} className={props.className}>
-      <ModalBody className={props.className}>
-        <iframe title="My Resume" className="pdf-viewer" src={props.file} />
-      </ModalBody>
-    </Modal>
-  </div>
-);
+export default (props) => {
+  let pdfURL = props.file;
+  if (/^http.*?github\.io/.test(document.URL)) {
+    pdfURL = `https://raw.githubusercontent.com/semlak/react-portfolio/gh-pages/${props.file}`;
+  }
+  console.log('hey, pdfURL', pdfURL);
+
+  return (
+    <div className="pdf-modal-container">
+      <Modal size="lg" isOpen={props.isOpen} toggle={props.toggle} className={props.className}>
+        <ModalBody className={props.className}>
+          <iframe title="My Resume" className="pdf-viewer" src={pdfURL} />
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+};
